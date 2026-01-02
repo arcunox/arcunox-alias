7015edae5d47b992f0dd0c7a4be5f43299a610a9
aedifx <252517075+aedifx@users.noreply.github.com>
2026-01-02 13:18:51 +1100

    Add files via upload

---
+++ vocab.js
@@ -0,0 +1,8 @@
+// vocab.js
+export const MODES = {
+  abstract: {
+    prefixes: ["ar", "vel", "rin", "sol", "kai", "vor", "ena"],
+    shards:   ["lin", "var", "sola", "rin", "kira", "mav", "taro"]
+  }
+};
+
